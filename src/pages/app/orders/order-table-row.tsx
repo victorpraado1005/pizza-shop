0ff2c9bd2c42102f29952e949fb1ce7bbb7e2d3f
Victor Prado <prado.victor1005@gmail.com>
2024-06-09 22:52:02 -0300

    criando menu de filtros da aplicacao.

---
+++ src/pages/app/orders/order-table-row.tsx
@@ -0,0 +1,39 @@
+import { ArrowRight, Search, X } from 'lucide-react'
+
+import { Button } from '@/components/ui/button'
+import { TableCell, TableRow } from '@/components/ui/table'
+
+export function OrderTableRow() {
+  return (
+    <TableRow>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <Search className="size-3" />
+          <span className="sr-only">Detalhes do pedidos</span>
+        </Button>
+      </TableCell>
+      <TableCell className="font-mono text-xs font-medium">321232</TableCell>
+      <TableCell className="text-muted-foreground">há 15 minutos</TableCell>
+      <TableCell>
+        <div className="flex items-center gap-2">
+          <span className="h-2 w-2 rounded-full bg-slate-400" />
+          <span className="font-medium text-muted-foreground">Pendente</span>
+        </div>
+      </TableCell>
+      <TableCell className="font-medium">Victor Prado</TableCell>
+      <TableCell className="font-medium">R$147,89</TableCell>
+      <TableCell>
+        <Button variant="outline" size="xs">
+          <ArrowRight className="mr-2 size-3" />
+          Aprovar
+        </Button>
+      </TableCell>
+      <TableCell>
+        <Button variant="ghost" size="xs">
+          <X className="mr-2 size-3" />
+          Cancelar
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
--- src/pages/app/orders/orders.tsx
+++ src/pages/app/orders/orders.tsx
@@ -1,16 +1,15 @@
-import { ArrowRight, Search, X } from 'lucide-react'
 import { Helmet } from 'react-helmet-async'
 
-import { Button } from '@/components/ui/button'
-import { Input } from '@/components/ui/input'
 import {
   Table,
   TableBody,
-  TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from '@/components/ui/table'
+
+import { OrderTableFilters } from './order-table-filters'
+import { OrderTableRow } from './order-table-row'
 
 export function Orders() {
   return (
@@ -20,10 +19,7 @@ export function Orders() {
         <h1 className="text-3xl font-bold tracking-tight">Pedidos</h1>
       </div>
       <div className="space-y-2.5">
-        <form className="flex items-center gap-2">
-          <span className="text-sm font-semibold">Filtros</span>
-          <Input placeholder="Nome do cliente" className="h-8 w-[320px]" />
-        </form>
+        <OrderTableFilters />
 
         <div className="rounded-md border">
           <Table>
@@ -40,42 +36,9 @@ export function Orders() {
               </TableRow>
             </TableHeader>
             <TableBody>
-              <TableRow>
-                <TableCell>
-                  <Button variant="outline" size="xs">
-                    <Search className="size-3" />
-                    <span className="sr-only">Detalhes do pedidos</span>
-                  </Button>
-                </TableCell>
-                <TableCell className="font-mono text-xs font-medium">
-                  321232
-                </TableCell>
-                <TableCell className="text-muted-foreground">
-                  há 15 minutos
-                </TableCell>
-                <TableCell>
-                  <div className="flex items-center gap-2">
-                    <span className="h-2 w-2 rounded-full bg-slate-400" />
-                    <span className="font-medium text-muted-foreground">
-                      Pendente
-                    </span>
-                  </div>
-                </TableCell>
-                <TableCell className="font-medium">Victor Prado</TableCell>
-                <TableCell className="font-medium">R$147,89</TableCell>
-                <TableCell>
-                  <Button variant="outline" size="xs">
-                    <ArrowRight className="mr-2 size-3" />
-                    Aprovar
-                  </Button>
-                </TableCell>
-                <TableCell>
-                  <Button variant="ghost" size="xs">
-                    <X className="mr-2 size-3" />
-                    Cancelar
-                  </Button>
-                </TableCell>
-              </TableRow>
+              {Array.from({ length: 10 }).map((_, i) => {
+                return <OrderTableRow key={i} />
+              })}
             </TableBody>
           </Table>
         </div>
